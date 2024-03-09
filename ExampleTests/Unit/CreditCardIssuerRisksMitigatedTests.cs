4bca3eafa63b14ddab2c6c4c9a1546f1e842eaca
Dmitri Kuznetsov <kna_int@mail.ru>
2024-03-09 22:04:13 +0200

    Added initial tests

--- ExampleTests/Unit/CreditCardIssuerRisksMitigatedTests.cs
+++ ExampleTests/Unit/CreditCardIssuerRisksMitigatedTests.cs
@@ -1,17 +1,35 @@
 using Examples.CreditCard.RisksMitigated;
 using Microsoft.Extensions.Time.Testing;
+using Moq;
 
 namespace ExampleTests.Unit
 {
     public class CreditCardIssuerRisksMitigatedTests
     {
         private TimeProvider _timeProvider;
+
+        private Mock<ICreditCardNumberGenerator> _creditCardNumberGeneratorMock;
         private ICreditCardNumberGenerator _creditCardNumberGenerator;
+
+        private Mock<ICreditCardCVCGenerator> _creditCardCVCGeneratorMock;
         private ICreditCardCVCGenerator _creditCardCVCGenerator;
 
         public CreditCardIssuerRisksMitigatedTests()
         {
             _timeProvider = new FakeTimeProvider();
+        }
+
+        private void SetupMocks()
+        {
+            var cardNumber = "4242424242424242";
+
+            _creditCardNumberGeneratorMock = new Mock<ICreditCardNumberGenerator>();
+            _creditCardNumberGeneratorMock.Setup(m => m.Generate()).Returns(cardNumber);
+            _creditCardNumberGenerator = _creditCardNumberGeneratorMock.Object;
+
+            _creditCardCVCGeneratorMock = new Mock<ICreditCardCVCGenerator>();
+            _creditCardCVCGeneratorMock.Setup(m => m.Generate(cardNumber)).Returns("123");
+            _creditCardCVCGenerator = _creditCardCVCGeneratorMock.Object;
         }
 
         [Fact]
@@ -20,12 +38,15 @@ namespace ExampleTests.Unit
             // Arrange
             var firstName = "First";
             var lastName = "Last";
+            SetupMocks();
             var issuer = new CreditCardIssuerRisksMitigated(_timeProvider, _creditCardNumberGenerator, _creditCardCVCGenerator);
 
             // Act
             var card = issuer.IssueCard(firstName, lastName);
 
             // Assert
+            _creditCardCVCGeneratorMock.VerifyAll();
+            _creditCardNumberGeneratorMock.VerifyAll();
             Assert.Equal(firstName, card.FirstName);
             Assert.Equal(lastName, card.LastName);
         }
