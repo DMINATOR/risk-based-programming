e90babc2cca5d283ac492428fc061b4403c3b770
Dmitri Kuznetsov <kna_int@mail.ru>
2024-03-10 19:24:41 +0200

    Added iterations for testing

--- ExampleTests/Integration/StartupCreditCardIssuerFixedTests.cs
+++ ExampleTests/Integration/StartupCreditCardIssuerFixedTests.cs
@@ -13,22 +13,26 @@ namespace ExampleTests.Integration
             _serviceProvider = services.BuildServiceProvider();
         }
 
-        [Fact]
-        public void StartupCreditCardIssuerFixedTests_IssueCard_Default_Success()
+        [Theory]
+        [InlineData(10)]
+        public void StartupCreditCardIssuerFixedTests_IssueCard_Default_Success(int iterations)
         {
             // Arrange
             var now = DateTime.UtcNow;
 
-            // Act
-            var card = StartupCardIssuerFixed.IssueCard(_serviceProvider);
+            for(var i = 0; i < iterations; i++)
+            {
+                // Act
+                var card = StartupCardIssuerFixed.IssueCard(_serviceProvider);
 
-            // Assert
-            Assert.Equal("name1", card.FirstName);
-            Assert.Equal("name2", card.LastName);
-            Assert.Equal(16, card.Number.Length);
-            Assert.Equal(3, card.CVC.Length);
-            Assert.Equal(now.Month, card.ValidMonth);
-            Assert.Equal(now.Year + 5, card.ValidYear); // with default validity years
+                // Assert
+                Assert.Equal("name1", card.FirstName);
+                Assert.Equal("name2", card.LastName);
+                Assert.Equal(16, card.Number.Length);
+                Assert.Equal(3, card.CVC.Length);
+                Assert.Equal(now.Month, card.ValidMonth);
+                Assert.Equal(now.Year + 5, card.ValidYear); // with default validity years
+            }
         }
     }
 }
