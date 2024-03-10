eaa432a63ce22b040ea8bd5f301492b0742c0121
Dmitri Kuznetsov <kna_int@mail.ru>
2024-03-10 20:12:54 +0200

    Added cat download

---
+++ ExampleTests/Integration/DownloadJsonTests.cs
@@ -0,0 +1,52 @@
+﻿using Examples.JSON;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace ExampleTests.Integration
+{
+    public class DownloadJsonTests
+    {
+        ServiceProvider _serviceProvider;
+        private readonly ITestOutputHelper _output;
+
+        // Values
+        private string EndpointUrlSource = "https://meowfacts.herokuapp.com/"; // Random cat fact
+        private string DownloadFileName = $"randomcatfact-{Guid.NewGuid()}.json";
+
+        public DownloadJsonTests(ITestOutputHelper output)
+        {
+            _output = output;
+
+            var services = new ServiceCollection();
+            services.AddHttpClient();
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        [Fact]
+        public async Task StartupCreditCardIssuerFixedTests_IssueCard_Default_Success()
+        {
+            // Arrange
+            var download = new DownloadJson(_serviceProvider.GetRequiredService<IHttpClientFactory>());
+
+            // Act
+            var directory = Directory.CreateTempSubdirectory();
+            var path = Path.Combine(directory.FullName, DownloadFileName);
+            await download.DownloadAsync(EndpointUrlSource, path);
+
+            // Assert
+            _output.WriteLine($"Stored file '{EndpointUrlSource}' -> '{path}'");
+
+            var contents = File.ReadAllText(path);
+            _output.WriteLine($"'{contents}'");
+
+            Assert.Contains("data", contents);
+        }
+    }
+}
---
+++ Examples/JSON/DownloadJson.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.JSON
+{
+    public class DownloadJson
+    {
+        private IHttpClientFactory _clientFactory;
+
+        public DownloadJson(IHttpClientFactory httpClientFactory)
+        {
+            _clientFactory = httpClientFactory;
+        }
+
+        public async Task DownloadAsync(string endpointUrl, string outputPath)
+        {
+            var httpClient = _clientFactory.CreateClient();
+
+            try
+            {
+                // Download JSON data from the API
+                string jsonData = await httpClient.GetStringAsync(endpointUrl);
+
+                // Save the JSON data to a file
+                File.WriteAllText(outputPath, jsonData);
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error downloading JSON: {ex.Message}");
+            }
+        }
+    }
+}
